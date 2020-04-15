b189dc11822d40b1e68b67f11b64b2789f421a8c
Daniel Graham <daniel@Daniels-MacBook-Pro.local>
2020-04-15 16:01:02 +0100

    Random change

--- pages/index.js
+++ pages/index.js
@@ -18,8 +18,8 @@ const Home = () => (
 
       <div className="grid">
         <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API. Eats</p>
+          <h3>Food &rarr;</h3>
+          <p>Find in-depth information about food around you</p>
         </a>
 
         <a href="https://nextjs.org/learn" className="card">
