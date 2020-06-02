fd0ad814da58594a8d65b0c908165d7be77e8ca6
Daniel Graham <daniel@Daniels-MacBook-Pro.local>
2020-06-02 18:40:21 +0100

    First Website Change

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      'sans': ['Helvetica', 'Arial', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
