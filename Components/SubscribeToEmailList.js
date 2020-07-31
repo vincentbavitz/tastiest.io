9d1f40d03e8e90d04ba5e564019465b839ac52b8
Daniel Graham <daniel@Daniels-MacBook-Pro.local>
2020-07-31 18:11:11 +0100

    make input form autocapitalize="none"

--- Components/SubscribeToEmailList.js
+++ Components/SubscribeToEmailList.js
@@ -10,7 +10,7 @@ function SubscribeToEmailList() {
                         <h1 className='text-center text-2xl font-black mb-5'>Only the best dishes.</h1>
                         <form>
                             <div className='flex justify-center rounded-t-md mx-8 my-0'>
-                                    <input className="bg-white border-orange-400 rounded-t-md w-full py-4 px-8 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-orange-500" placeholder="email..."/>                             
+                                    <input autocapitalize="none" className="bg-white border-orange-400 rounded-t-md w-full py-4 px-8 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-orange-500" placeholder="email..."/>                             
                             </div>
                             <div class="flex justify-center my-0 mb-5 mx-8">
                                 <button class="bg-primary hover:bg-orange-400 focus:shadow-outline focus:outline-none text-white font-bold py-4 px-24 rounded-b-md shadow-lg text-lg" type="button">
