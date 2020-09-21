0a87f632122d3111e77bf2220f1f268717041124
Vince <vincent@bavitz.org>
2020-09-21 22:11:11 +1000

    🍫 Cuisine bar styling

--- components/CuisineBar.tsx
+++ components/CuisineBar.tsx
@@ -14,39 +14,34 @@ interface Props {
 function CuisineItem(props: Props) {
   return (
     <Link href={props.href}>
-      <div
-        className={classNames(
-          'menu-item',
-          'overflow-y-visible',
-          'rounded-xl',
-          'pr-12',
-          'pl-4',
-          'py-3',
-          'mr-4',
-          'inline-flex',
-          'items-center',
-          'justify-between',
-          'flex-1',
-          'text-sm',
-          'font-bold',
-          'px-2',
-          'py-2',
-          'mx-3',
-          'mt-4',
-          'border-2',
-          'rounded-lg',
-          'bg-white',
-          'text-secondary',
-          'border-primary',
-          'hover:text-secondary-300',
-          'hover:bg-transparent',
-          'lg:mt-0',
-          'first:ml-0',
-          'last:mr-0',
-        )}
-      >
-        <span className="pr-3">{props.name}</span>
-        {props.svg}
+      <div className="cuisine-item flex py-6">
+        <div
+          className={classNames(
+            'overflow-y-visible',
+            'rounded-xl',
+            'pl-4',
+            'py-3',
+            'inline-flex',
+            'items-center',
+            'justify-between',
+            'flex-1',
+            'text-sm',
+            'font-bold',
+            'pl-2',
+            'py-2',
+            'border-2',
+            'rounded-lg',
+            'bg-white',
+            'text-secondary',
+            'border-primary',
+            'hover:text-secondary-300',
+            'hover:bg-transparent',
+            'lg:mt-0',
+          )}
+        >
+          <span className="pr-1">{props.name}</span>
+          {props.svg}
+        </div>
       </div>
     </Link>
   );
@@ -54,8 +49,8 @@ function CuisineItem(props: Props) {
 
 export function CuisineBar() {
   return (
-    <div className="w-full overflow-y-visible">
-      <div className="flex px-5 md:px-0 overflow-y-visible scrolling-touch hide_scroll">
+    <div className="w-full overflow-x-scroll hide_scroll">
+      <div className="flex md:px-0 overflow-y-visible scrolling-touch">
         <div className="flex w-full overflow-y-visible">
           {cuisines.map(cuisine => (
             <CuisineItem
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
   },
   variants: {
-    margin: ['responsive', 'first'],
+    margin: ['responsive', 'first', 'last'],
   },
   plugins: [],
 };
