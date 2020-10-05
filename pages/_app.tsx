6a4abc7330b9e6adf86c638086db1e6dfde14af3
Daniel Graham <daniel@Daniels-MacBook-Pro.local>
2020-10-05 12:47:05 +0100

    pw page styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { rootReducer } from '../state/reducers';
+import TastiestLogo from '../assets/svgs/brand.svg';
 
 const store = createStore(rootReducer);
 
@@ -21,9 +22,20 @@ function App({ Component, pageProps }: AppProps) {
       {isLoggedIn ? (
         <Component {...pageProps} />
       ) : (
-        <div className="flex justify-center items-center">
-          <h1>Enter the password to continue</h1>
-          <input className="bg-gray-300" onChange={handleOnChange} />
+        <div className="flex justify-center items-center absolute top-0 bottom-0 left-0 right-0">
+          <div className="flex flex-col justify-center -m-8 items-center">
+            <TastiestLogo className="h-12 my-6" />
+            <p className="font-robotolight text-base">
+              Enter the password to continue:
+            </p>
+            <input
+              className="my-2 border-secondary border-2 rounded-lg focus:outline-none focus:border-primary pl-2 py-1"
+              onChange={handleOnChange}
+            />
+            <h1 className="font-somantic text-secondary text-2xl pt-8">
+              Discover. Eat. Smile.
+            </h1>
+          </div>
         </div>
       )}
     </Provider>
