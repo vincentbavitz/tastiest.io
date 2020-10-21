8d36ed5ceacbf72a549627b49d3d2cfbaf1e0ac9
Vince <vincent@bavitz.org>
2020-10-21 22:59:49 +1100

    Firebase initialised

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
+import { Button } from '../components/Button';
 import { Contained } from '../components/Contained';
 import { Input } from '../components/Input';
-import { getUsers } from '../utils/login';
 
 export default function Login() {
-  getUsers();
-
   return (
     <Contained>
       <div className="flex flex-col space-y-5 w-1/2 mb-24">
@@ -22,12 +20,10 @@ export default function Login() {
           <Input />
         </div>
         <div>
-          Sign in with Google
-          <Input />
+          <Button size="small">Sign in with Google</Button>
         </div>
         <div>
-          Sign in with Facebook
-          <Input />
+          <Button size="small">Sign in with Facebook</Button>
         </div>
       </div>
     </Contained>
