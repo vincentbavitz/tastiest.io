61c45082a9219423413ff056a17d0175578dc072
Vince <vincent@bavitz.org>
2020-10-25 22:59:29 +1100

    isLoading on login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app';
-import { useRouter } from 'next/dist/client/router';
 import Head from 'next/head';
 import React from 'react';
 import { Provider } from 'react-redux';
@@ -7,42 +6,12 @@ import { createStore } from 'redux';
 import '../assets/style.scss';
 import { CuisineBar } from '../components/CuisineBar/CuisineBar';
 import NavBar from '../components/NavBar';
-import { SearchOverlay } from '../components/search/SearchOverlay';
 import { METADATA } from '../constants';
 import { rootReducer } from '../state/reducers';
 
 const store = createStore(rootReducer);
 
 function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  // const [isVerified, setIsVerified] = useState(true);
-
-  // const inputRef = useRef(null);
-  // const password = 'tastiest';
-
-  // // Privacy page always visible
-  // useEffect(() => {
-  //   if (window.location.href.includes('privacy')) {
-  //     setIsVerified(true);
-  //   } else {
-  //     setIsVerified(false);
-  //   }
-  // }, [router.pathname]);
-
-  // // Focus on mount
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     inputRef?.current?.focus();
-  //   }, 0);
-  // }, []);
-
-  // const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
-  //   if (e.target.value.toLowerCase() === password) {
-  //     setIsVerified(true);
-  //   }
-  // };
-
   return (
     <Provider store={store}>
       <Head>
@@ -52,35 +21,9 @@ function App({ Component, pageProps }: AppProps) {
       <>
         <NavBar />
         <CuisineBar />
-        <SearchOverlay />
+        {/* <SearchOverlay /> */}
         <Component {...pageProps} />
       </>
-
-      {/* {isVerified ? (
-        <>
-          <NavBar />
-          <CuisineBar />
-          <SearchOverlay />
-          <Component {...pageProps} />
-        </>
-      ) : (
-        <div className="flex justify-center items-center absolute top-0 bottom-0 left-0 right-0">
-          <div className="flex flex-col justify-center -m-8 items-center">
-            <TastiestLogo className="h-12 my-6" />
-            <p className="font-robotolight text-base">
-              Enter the password to continue
-            </p>
-            <input
-              ref={inputRef}
-              className="my-2 border-secondary border-2 rounded-lg focus:outline-none focus:border-primary pl-2 py-1 text-center"
-              onChange={handleOnChange}
-            />
-            <h1 className="font-somantic text-secondary text-twoxl pt-8">
-              Discover. Eat. Smile.
-            </h1>
-          </div>
-        </div>
-      )} */}
     </Provider>
   );
 }
--- pages/account.tsx
+++ pages/account.tsx
@@ -3,6 +3,7 @@ import 'firebase/functions';
 import Link from 'next/link';
 import Router from 'next/router';
 import React, { useEffect } from 'react';
+import { Title } from '../components/Title';
 import initFirebase from '../utils/auth/initFirebase';
 import logout from '../utils/auth/logout';
 import withAuthUser from '../utils/pageWrappers/withAuthUser';
@@ -56,6 +57,8 @@ const Account = (props: any) => {
               [ log out ]
             </button>
           </p>
+
+          <Title level={3}>Favourites</Title>
         </>
       )}
     </>
