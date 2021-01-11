2296bf28e9454124dcf0840dfbc407f63198a7a3
Vince <vincent@bavitz.org>
2021-01-11 13:50:38 +1100

    Firebase hooks

--- components/article/sections/ArticleSectionContent.tsx
+++ components/article/sections/ArticleSectionContent.tsx
@@ -2,15 +2,13 @@ import BlockContent from '@sanity/block-content-to-react';
 import React, { useContext } from 'react';
 import { SANITY_CONSTATNTS } from '../../../client';
 import { ScreenContext } from '../../../contexts/screen';
-import { IFigureImage } from '../../../types/article';
+import { IArticle } from '../../../types/article';
 import { Contained } from '../../Contained';
-import { ArticleWidgetOrderNow } from '../widgets/ArticleWidgetOrderNow';
+import {
+  ArticleWidgetOrderNow,
+  IOrderDeal,
+} from '../widgets/ArticleWidgetOrderNow';
 import { ArticleSectionFeatureImage } from './ArticleSectionFeatureImage';
-
-interface Props {
-  body: string | string[];
-  featureImage: IFigureImage;
-}
 
 const paragraphs = [
   `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, labore consequatur. Architecto, aperiam impedit. Corrupti inventore eius, exercitationem odit est fugit, iste aspernatur incidunt quod iure aliquid fugiat. Earum, voluptate. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, labore consequatur. Architecto, aperiam impedit. Corrupti inventore eius, exercitationem odit est fugit, iste aspernatur incidunt quod iure aliquid fugiat. Earum, voluptate. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, labore consequatur. Architecto, aperiam impedit. Corrupti inventore eius, exercitationem odit est fugit, iste aspernatur incidunt quod iure aliquid fugiat. Earum, voluptate. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, labore consequatur. Architecto, aperiam impedit.', 'Corrupti inventore eius, exercitationem
@@ -50,48 +48,65 @@ Architecto, aperiam impedit. Corrupti inventore eius, exercitationem
 odit est fugit, iste aspernatur incidunt quod iure aliquid fugiat.`,
 ];
 
+const deal: IOrderDeal = {
+  restaurantID: 'divine-steaks-market',
+  dealName: 'Grizzly Grumble Deal',
+  dealDescription: 'Experience the best porterhouse steak in London',
+  dealPrefix: "Only £25 and you'll get",
+  dealItems: [
+    '300g Porterhouse',
+    'Fries',
+    'Salad',
+    'Choice of steak sauces',
+    'House wine/beer',
+  ],
+  pricePerHeadGBP: 25,
+};
+
 // TODO
 // Really want writers to be able to do {{CALL_TO_ACTION}} sorta things like Wordpress shortcodes
 // And that will then conditionally render our components
-export function ArticleSectionContent(props: Props) {
+export function ArticleSectionContent(post: IArticle) {
   const { isDesktop } = useContext(ScreenContext);
 
   return (
     <Contained>
-      {isDesktop ? <DesktopContent {...props} /> : <MobileContent {...props} />}
+      {isDesktop ? <DesktopContent {...post} /> : <MobileContent {...post} />}
     </Contained>
   );
 }
 
-const MobileContent = ({ body, featureImage }: Props) => (
+const MobileContent = (post: IArticle) => (
   <div className="flex flex-col space-y-4">
     <div>{paragraphs[0]}</div>
 
-    <ArticleWidgetOrderNow />
+    <ArticleWidgetOrderNow {...deal} />
 
-    <div>{paragraphs[1]}</div>
+    <BlockContent
+      blocks={post.body}
+      projectId={SANITY_CONSTATNTS.PROJECT_ID}
+      dataset={SANITY_CONSTATNTS.DATASET}
+    />
 
-    <ArticleSectionFeatureImage featureImage={featureImage} />
-
-    <div>{paragraphs[1]}</div>
+    <ArticleSectionFeatureImage featureImage={post.featureImage} />
   </div>
 );
 
-const DesktopContent = ({ body, featureImage }: Props) => (
+const DesktopContent = (post: IArticle) => (
   <div className="flex flex-col">
     <div className="flex space-x-10">
       <div className="w-8/12 mt-16">
         <BlockContent
-          blocks={body}
+          blocks={post.body}
           projectId={SANITY_CONSTATNTS.PROJECT_ID}
           dataset={SANITY_CONSTATNTS.DATASET}
         />
       </div>
 
       <div className="w-4/12 mt-12">
-        <ArticleWidgetOrderNow />
+        <ArticleWidgetOrderNow {...deal} />
       </div>
     </div>
-    <ArticleSectionFeatureImage featureImage={featureImage} />
+    <ArticleSectionFeatureImage featureImage={post.featureImage} />
   </div>
 );
