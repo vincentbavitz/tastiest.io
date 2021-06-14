9fa508404725594ce25b99ce4f551c81efc51451
Vincent Bavitz <vincent@bavitz.org>
2021-06-14 06:32:22 -0400

    Fix cursor skipping on InputContactBirthday

--- components/inputs/contact/InputContactBirthday.tsx
+++ components/inputs/contact/InputContactBirthday.tsx
@@ -2,6 +2,7 @@ import { Input } from '@tastiest-io/tastiest-components';
 import { dlog, IDateObject } from '@tastiest-io/tastiest-utils';
 import React, { useState } from 'react';
 import NumberFormat, { NumberFormatValues } from 'react-number-format';
+import { useThrottle } from 'react-use';
 import { dateFormat, dateToString, stringToDate } from 'utils/text';
 
 interface Props {
@@ -32,9 +33,9 @@ const InputCustom = (props: Props) => {
       inputMode="decimal"
       color="secondary"
       inputClassName="font-mono w-full"
-      {...props}
       onBlur={validate}
       error={error}
+      {...props}
     />
   );
 };
@@ -45,13 +46,27 @@ export function InputContactBirthday(props: Props) {
     props.onDateChange(date);
   };
 
+  // Prevents input cursor from being set behind last position
+  // when inputting numbers very quickly
+  const throttledDate = useThrottle(props.date, 30);
+  const setCursor: React.ChangeEventHandler<HTMLInputElement> = event => {
+    setTimeout(() => {
+      const element = event.target;
+      window.requestAnimationFrame(() => {
+        element.selectionStart = element.value.length;
+        element.selectionEnd = element.value.length;
+      });
+    }, 50);
+  };
+
   return (
     <NumberFormat
       placeholder="DD/MM/YYYY"
       mask={['M', 'M', 'D', 'D', 'Y', 'Y', 'Y', 'Y']}
       format={value => dateFormat(value)}
       customInput={InputCustom}
-      value={dateToString(props.date)}
+      value={dateToString(throttledDate)}
+      onChange={setCursor}
       onValueChange={handleOnChange}
     />
   );
