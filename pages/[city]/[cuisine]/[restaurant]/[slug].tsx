6291c5e18a07e25c11ae1d962b24f46d0d26a617
Vincent Bavitz <vincent@bavitz.org>
2021-12-18 11:07:19 -0600

    Testt Horiz outide

--- pages/[city]/[cuisine]/[restaurant]/[slug].tsx
+++ pages/[city]/[cuisine]/[restaurant]/[slug].tsx
@@ -1,5 +1,6 @@
 import { CmsApi, dlog, ExperiencePost } from '@tastiest-io/tastiest-utils';
 import { ArticleSectionContent } from 'components/article/sections/ArticleSectionContent';
+import ExperienceOrderPanelInner from 'components/article/widgets/ExperienceOrderPanelInner';
 import { YouTubeVideo } from 'components/YouTubeVideo';
 import { useTrack } from 'hooks/useTrack';
 import { Layouts } from 'layouts/LayoutHandler';
@@ -157,6 +158,13 @@ function Experience(props: InferGetStaticPropsType<typeof getStaticProps>) {
             },
           ],
         }}
+      />
+
+      <ExperienceOrderPanelInner
+        posts={posts}
+        deal={post.deal}
+        slug={post.slug}
+        layout="overlay"
       />
 
       <div className="relative w-full">
