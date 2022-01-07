483cd550575ead7278d2ed1c7eaff88eedb1979a
Vincent Bavitz <vincent@bavitz.org>
2022-01-07 12:20:11 -0600

    Following Modal

--- components/restaurant/FollowButton.tsx
+++ components/restaurant/FollowButton.tsx
@@ -1,9 +1,8 @@
-import { BellOutlined, PlusOutlined } from '@ant-design/icons';
-import { Button, ButtonGroup } from '@tastiest-io/tastiest-ui';
-import { dlog, RestaurantContentful } from '@tastiest-io/tastiest-utils';
-import clsx from 'clsx';
+import { PlusOutlined } from '@ant-design/icons';
+import { Button, Modal } from '@tastiest-io/tastiest-ui';
+import { RestaurantContentful } from '@tastiest-io/tastiest-utils';
 import useFollow from 'hooks/useFollow';
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
   restaurant: RestaurantContentful;
@@ -22,41 +21,84 @@ export default function FollowButton(props: Props) {
     notificationsLoading,
   } = useFollow(restaurant.id);
 
-  dlog('FollowButton ➡️ restaurant.id:', restaurant.id);
+  const [showExplanationModal, setShowExplanationModal] = useState(false);
+  const toggleFollowRestaurant = (shouldFollow: boolean) => {
+    setShowExplanationModal(false);
+
+    if (shouldFollow) {
+      follow();
+    } else {
+      unfollow();
+    }
+  };
 
   return (
-    <div className="">
-      {following ? (
-        <ButtonGroup>
-          <Button loading={followLoading} onClick={unfollow}>
-            Following
+    <>
+      <Modal title="How Following Works" show={showExplanationModal}>
+        <p className="mb-2">You'll be emailed when the restaurant...</p>
+        <ol className="list-inside list-disc">
+          <li>has a new experience, such as a new menu or dish</li>
+          <li>has a last-minute cancellaton at a popular time</li>
+          <li>sends out other updates</li>
+        </ol>
+
+        <div className="flex justify-end space-x-2">
+          <Button color="light" onClick={() => setShowExplanationModal(false)}>
+            Back
           </Button>
 
           <Button
-            color={'secondary'}
-            onClick={() => toggleNotifications(!notifications)}
-            loading={notificationsLoading}
+            onClick={() => toggleFollowRestaurant(!following)}
+            loading={followLoading}
           >
-            <div className="flex items-center">
-              <BellOutlined
-                className={clsx(
-                  'duration-300 text-lg',
-                  notifications ? 'text-white' : 'text-gray-200',
-                )}
-              />
-            </div>
+            {following ? 'Unfollow' : 'Follow'}
           </Button>
-        </ButtonGroup>
-      ) : (
-        <Button
-          color={'light'}
-          onClick={follow}
-          prefix={<PlusOutlined />}
-          loading={followLoading}
+        </div>
+      </Modal>
+
+      <div className="flex items-center -mr-7 space-x-2">
+        {following ? (
+          <Button loading={followLoading} onClick={unfollow}>
+            Following
+          </Button>
+        ) : (
+          // <ButtonGroup>
+          //   <Button loading={followLoading} onClick={unfollow}>
+          //     Following
+          //   </Button>
+
+          //   <Button
+          //     color={'secondary'}
+          //     onClick={() => toggleNotifications(!notifications)}
+          //     loading={notificationsLoading}
+          //   >
+          //     <div className="flex items-center">
+          //       <BellOutlined
+          //         className={clsx(
+          //           'duration-300 text-lg',
+          //           notifications ? 'text-white' : 'text-gray-200',
+          //         )}
+          //       />
+          //     </div>
+          //   </Button>
+          // </ButtonGroup>
+          <Button
+            color={'light'}
+            onClick={() => setShowExplanationModal(true)}
+            prefix={<PlusOutlined />}
+            loading={followLoading}
+          >
+            Follow
+          </Button>
+        )}
+
+        <div
+          onClick={() => setShowExplanationModal(true)}
+          className="flex items-center justify-center w-5 h-5 rounded-full bg-white filter drop-shadow-md font-primary cursor-pointer"
         >
-          Follow
-        </Button>
-      )}
-    </div>
+          i
+        </div>
+      </div>
+    </>
   );
 }
