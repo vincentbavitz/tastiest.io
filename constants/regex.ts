de40b1fd93fee762962fefb98359f50dc4660407
Vincent Bavitz <vincent@bavitz.org>
2022-01-18 23:20:50 -0600

    Fix UK postcode optional space

--- constants/regex.ts
+++ constants/regex.ts
@@ -1,6 +1,7 @@
 const REGEX = {
   EMAIL: /^[\w.-_]{1,30}@[\w\-_]{1,30}\.[a-zA-Z]{2,10}(\.[a-zA-Z]{2,10})?$/,
   WEBSITE: /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*(\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*)?$/,
+  POSTCODE_UK: /^([Gg][Ii][Rr] ?0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([AZa-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) ?[0-9][A-Za-z]{2})$/,
 };
 
 export default REGEX;
