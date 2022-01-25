51a0ae53c1076dcb800dd49f530a8f09ad2b90fd
Vincent Bavitz <vincent@bavitz.org>
2022-01-25 05:00:12 -0600

    NextJS Sitemaps

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+// Configuration taken from...
+// https://www.iamvishnusankar.com/blogs/how-to-generate-sitemap-for-nextjs-projects/
+
+module.exports = {
+  siteUrl: 'https://tastiest.io',
+  generateRobotsTxt: true,
+};
